0330432ac1c8f42913a7ea462b890482c168ed44
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-08-01 23:49:12 -0500

    proyecto taller

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Tarea } from './../../models/tareas.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  tareas: Tarea[] = [];
+  nuevaTarea: string = '';
+  idTarea = 0;
+  @ViewChild('tareaInput', { static: true }) tareaInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('elemento') elemento!: ElementRef;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  addTarea(event: any) {
+    const nuevaTareaValue = this.tareaInput.nativeElement.value.trim();
+    if (nuevaTareaValue !== '') {
+      this.idTarea++;
+      const nuevaTarea: Tarea = {
+        id: this.idTarea,
+        title: nuevaTareaValue,
+        completed: false,
+      };
+      this.tareas.push(nuevaTarea);
+      this.nuevaTarea = '';
+      // Limpiar el input después de agregar la tarea
+      this.tareaInput.nativeElement.value = '';
+    }
   }
-
+  completedOrPending(tarea:Tarea) {
+    tarea.completed;
+  }
+  modeEdition(elementLi: HTMLElement){
+    elementLi.classList.add('editing');
+  }
 }
