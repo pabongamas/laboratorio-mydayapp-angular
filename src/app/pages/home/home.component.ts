98798d14ee8504028f92ea3e1bcf7db81a6a559a
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-08-03 21:58:49 -0500

    actualizacion laboratorio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Tarea } from './../../models/tareas.model';
 import {TaskServicesService} from './../../services/task-services.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,26 @@ import {TaskServicesService} from './../../services/task-services.service';
 export class HomeComponent{
   tasks: Tarea[] = [];
   taskTitle:string="";
+  currentRoute: string = '';
+  sub;
   constructor(
+    private route: ActivatedRoute,
     private taskService:TaskServicesService
   ) {
     this.tasks = this.taskService.getTasks();
+    this.sub = this.route.url.subscribe(url => {
+      console.log("y entonces: ", url);
+      if (url.length === 0) {
+        this.currentRoute = 'all';
+        console.log(this.currentRoute);
+      } else {
+        this.currentRoute = url[0].path;
+      }
+      // console.log(url);
+      // this.currentRoute = url[0].path;
+      console.log(this.currentRoute);
+      this.filterTasks(this.currentRoute);
+    });
   }
  
 
@@ -33,5 +50,22 @@ export class HomeComponent{
   }
   eraseCompleted(){
     this.tasks = this.taskService.eraseCompleted();
+  }
+  filterTasks(filter: string) {
+    switch (filter) {
+      case 'all':
+        this.tasks = this.taskService.getTasks();
+        break;
+      case 'pending':
+        this.tasks = this.taskService.getTasks().filter(task => !task.completed);
+        console.log("estoy en peding", this.tasks);
+        break;
+      case 'completed':
+        this.tasks = this.taskService.getTasks().filter(task => task.completed);
+        break;
+      default:
+        this.tasks = this.taskService.getTasks();
+        break;
+    }
   }
 }
